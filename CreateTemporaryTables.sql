e2484b7e17c36a8c2f78705fd4b4973b87e14325
Aleksandra Hinc <olka.hinc@gmail.com>
2026-03-07 22:29:59 +0100

    Update CreateTemporaryTables.sql

--- CreateTemporaryTables.sql
+++ CreateTemporaryTables.sql
@@ -3,8 +3,16 @@ WITH SubscriptionsPerCustomer AS (
 SELECT CustomerId, COUNT(SubscriptionId) AS 'Subscriptions'
 FROM Orders
 GROUP BY 1
+),
+-- Adding a new record
+NewRecord AS (
+	SELECT 'Tony Smith', 5 AS 'Subscriptions'
 )
-SELECT Customers.Name, SubscriptionsPerCustomer.Subscriptions
-FROM SubscriptionsPerCustomer
-JOIN Customers
-ON SubscriptionsPerCustomer.CustomerId = Customers.Id;
+-- UNION
+SELECT C.Name, SPC.Subscriptions
+FROM SubscriptionsPerCustomer SPC
+JOIN Customers C
+ON SPC.CustomerId = C.Id
+UNION
+SELECT *
+FROM NewRecord;
