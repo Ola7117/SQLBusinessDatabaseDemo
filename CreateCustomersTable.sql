f40a4a1b8d514f814263b025073a69a4f463f0b3
Aleksandra Hinc <olka.hinc@gmail.com>
2026-03-07 20:51:49 +0100

    Split SelectOrderFilter.sql into 2 scripts

---
+++ CreateCustomersTable.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS Customers;
+
+CREATE TABLE Customers(
+	Id INTEGER PRIMARY KEY,
+	Name TEXT UNIQUE NOT NULL,
+	Age INTEGER,
+	Nationality TEXT DEFAULT 'Not Available'
+);
+
+SELECT *
+FROM Customers;
+
+INSERT INTO Customers (Id, Name, Age, Nationality)
+VALUES 
+(1, 'Jane Doe', 25, 'Canada'),
+(2, 'Jack Johnson', 45, 'United States'),
+(3, 'John Dow', 33, 'United States'),
+(4, 'Marie Maden', 24, 'England'),
+(5, 'Marie Malen', 55, 'Norway'),
+(6, 'Simon Letterman', 21, 'England'),
+(7, 'Mike Hannigan', 46, 'United States'),
+(8, 'Monica Geller', 41, 'United States'),
+(9, 'Jaime Calabro', 67, 'Mexico'),
+(10, 'Jose Gomez', 32, 'Mexico'),
+(11, 'Liam Nikon', NULL, 'Jamaica'),
+(12, 'Mat Diamond', NULL, 'Italy');
